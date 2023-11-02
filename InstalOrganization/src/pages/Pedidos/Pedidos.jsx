11c7c61fad365385f14824f493c69f97c04ccc1e
Douglas Araujo <testecliente207@gmail.com>
2023-11-02 15:37:10 -0300

    settings in pedido's page

--- InstalOrganization/src/pages/Pedidos/Pedidos.jsx
+++ InstalOrganization/src/pages/Pedidos/Pedidos.jsx
@@ -14,7 +14,8 @@ const Pedidos = () => {
                 />
                 <Button text="Novo Pedido" />
             </div>
-            <div className=" mx-4 my-6 p-2 rounded-sm w-auto h-40 bg-zinc-100">
+            <div className=" mx-4 my-6 p-2 rounded-sm w-auto h-[500px] bg-zinc-100">
+                <h1 className="font-semibold text-center p-2 border-solid border-b-2 border-[#494949]">Historico de Pedidos</h1>
                 <div className="mt-2 p-2 gap-2 bg-[#494949] w-full flex justify-between border-b-2 border-solid border-zinc-100 text-white">
                     <ul className="w-[10%] px-2 py-1 text-center">11/10/2023</ul>
                     <ul className="w-[50%] px-4 py-1">Posto matriz</ul>
