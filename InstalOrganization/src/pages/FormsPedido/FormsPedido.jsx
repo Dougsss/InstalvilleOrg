c2d5e214c8580b63c3c4b76f3481f4b948100cf7
Douglas Araujo <testecliente207@gmail.com>
2023-11-20 20:28:19 -0300

    any changes

--- InstalOrganization/src/pages/FormsPedido/FormsPedido.jsx
+++ InstalOrganization/src/pages/FormsPedido/FormsPedido.jsx
@@ -37,7 +37,7 @@ const FormsPedido = ({ isOpen, closeModal}) => {
             <div className="mx-4 my-6 p-4 w-auto bg-zinc-100 shadow-xl rounded-sm">
                 <h1 className="font-semibold text-center p-2 border-solid border-b-2 border-[#494949]">Novo Pedido</h1>
                         <form className="mt-4" onSubmit={handleSubmit}>
-                        <div className="mb-4">
+                        <div className="mb-4 w-[20%]">
                             <label className="block text-gray-700 text-sm font-bold mb-2">
                                 Data:
                             </label>
@@ -51,7 +51,7 @@ const FormsPedido = ({ isOpen, closeModal}) => {
                                 required
                             />
                         </div>
-                        <div className="mb-4">
+                        <div className="mb-4 w-[80%]">
                             <label className="block text-gray-700 text-sm font-bold mb-2">
                                 Nome:
                             </label>
@@ -95,7 +95,8 @@ const FormsPedido = ({ isOpen, closeModal}) => {
                                 />
                             </div>
                         </div>
-                        <div className="mb-4">
+                        {/* produtos */}
+                        <div className="mb-4 w-[50%]">
                             <label className="block text-gray-700 text-sm font-bold mb-2">
                                 Produto:
                             </label>
@@ -109,7 +110,8 @@ const FormsPedido = ({ isOpen, closeModal}) => {
                                 required
                             />
                         </div>
-                        <div className="mb-4 w-[25%]">
+                        <div className=" flex gap-5">
+                        <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2">
                                 Quantidade:
                             </label>
@@ -121,8 +123,7 @@ const FormsPedido = ({ isOpen, closeModal}) => {
                                 onChange={handleInputChange}
                                 className="w-full p-2 border border-solid border-[#D06610] rounded text-center"
                             />
-                        </div>
-                        <div className=" flex gap-5">
+                            </div>
                             <div className="mb-4">
                                 <label className="block text-gray-700 text-sm font-bold mb-2">
                                     Valor:
@@ -136,22 +137,10 @@ const FormsPedido = ({ isOpen, closeModal}) => {
                                     className="w-full p-2 border border-solid border-[#D06610] rounded"
                                 />
                             </div>
-                            <div className="mb-6">
-                                <label className="block text-gray-700 text-sm font-bold mb-2">
-                                    Valor Total:
-                                </label>
-                                <input
-                                    type="double"
-                                    name="valorTotal"
-                                    placeholder="Valor total dos Itens"
-                                    value={formData.valorTotal}
-                                    onChange={handleInputChange}
-                                    className="w-full p-2 border border-solid border-[#D06610] rounded"
-                                />
-                            </div>
                         </div>
-                        <div className="text-center">
-                            <Button text="Enviar" type="submit" />
+                        <div className="flex flex-row-reverse">
+                            <button className="bg-red-700 text-white p-2 rounded mx-1" onClick={closeModal}>Cancelar</button>
+                            <button className="bg-green-700 text-white p-2 rounded mx-1" type="submit">Adidcionar</button>
                         </div>
                         </form>
             </div>            
